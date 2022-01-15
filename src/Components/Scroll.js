abd21d0d20487a9ad853e57d556325bf256ac431
Logan Manor <lmmanor192@outlook.com>
2022-01-15 17:36:05 -0500

    having fun

--- src/Components/Scroll.js
+++ src/Components/Scroll.js
@@ -5,7 +5,7 @@ const Scroll = (props) => {
         <div style={{overflowY: 'scroll', border: '1px solid black', height: '500px'}}>
             {props.children}
         </div>
-    )
+    );
 };
 
 export default Scroll;
